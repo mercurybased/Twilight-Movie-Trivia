90f9b01fbf1e2a66c033e6c2301497a813c8a807
Anna <127552050+mercurybased@users.noreply.github.com>
2023-03-31 10:55:40 -0700

    added outlines to js and html

---
+++ assets/script.js
@@ -0,0 +1,82 @@
+// define all questions and answers
+
+// define variable for tracking
+//      track time
+//      track questions
+
+// create variables to reference DOM elements
+//      timer
+//      questions
+//      answers
+//      start button
+//      name/initials
+//      save button
+//      high scores container
+
+// function
+//      start quiz
+//          hide start button
+//          show the quiz container
+//          display first question
+//          start timer
+//          display countdown on screen
+
+// function
+//      rendering a question
+//          clear/remove previous question
+//          getting first question
+//          add question to the question container
+//          make button for each answer
+//          add answers to the answers container
+
+// function
+//      handle quiz completion
+//          stop timer
+//          hide quiz container
+//          show end screen
+//          show time remaining as score
+
+// function
+//      handle answer clicks
+//
+//      if
+//      answer is WRONG
+//          subtract time from timer
+//          make sure time is displayed correctly on page
+//          flash wrong answer message (setTimeout)
+//
+//      update question variable to next question
+//      display question on page
+//
+//      if
+//      question is the last question
+//      trigger quiz completion
+
+// function
+//      tracking time
+//          subtract time
+//          update the page
+//
+//      if
+//      timer hits zero
+//          trigger quiz completion
+
+// function
+//      saving high scores
+//          get value of user input (name/initals)
+//          validate input
+//          retreive existing data from local storage
+//          update the high score data
+//          save updated data back to local storage
+//          redirect to start screen after save
+
+// function
+//      listening for key press
+//          check if the key pressed was 'Enter' for saving scores
+//          OPTIONAL check if the key pressed was 'a', 'b', 'c' for answers
+
+// event listeners
+//      click start
+//      click answers
+//      click save score
+//      keyups
